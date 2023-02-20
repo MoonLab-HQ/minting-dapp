02d49c43ea4d16d3c85abaa357bb7918449c7e59
Bradley <bradley.t.sheehan@gmail.com>
2023-02-19 23:51:29 -0700

    prepay config price env

--- src/App.js
+++ src/App.js
@@ -334,10 +334,10 @@ function App() {
                           clientId="699b1efb-b821-41c0-bd8f-3d7deca42dd0"
                           mintConfig={{
                             type: "erc-721",
-                            totalPrice: "0.01",
+                            totalPrice: "0.045",
                             _mintAmount: "1",
                           }}
-                          environment="staging"
+                          environment="production"
                         />
 
                         <s.SpacerLarge />
