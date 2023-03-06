7fd66c107300ff12abadfd719bba9590dcb5e116
Bradley <bradley.t.sheehan@gmail.com>
2023-03-06 12:40:27 -0700

    update crossmint config

--- src/mint.js
+++ src/mint.js
@@ -1,23 +1,9 @@
 import { CrossmintPayButton } from "@crossmint/client-sdk-react-ui";
-
-    
-
     export default function Mint() {
-
         return (
-
             <CrossmintPayButton
- 
-                clientId="699b1efb-b821-41c0-bd8f-3d7deca42dd0"
-
-                mintConfig={{"type":"erc-721","totalPrice":"0.045","quantity":"1"}}
-
-                environment="production"
-
-                
-
+                clientId="1d16c0ac-02c0-4dfc-b834-acaf6781cbc1"
+                mintConfig={{"type":"erc-721","totalPrice":"0.045"}}
             />
-
         );
-
     }
